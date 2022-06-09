7496fe86502ddd3cd02b23bddcd0b7e018b6c726
jonasmkunzler <jonasmkunzler@gmail.com>
2022-06-09 10:31:28 -0300

    1 file modified

--- pages/signin.js
+++ pages/signin.js
@@ -7,10 +7,24 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/styles';
+import { makeStyles } from '@material-ui/core/styles';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,7 +33,10 @@ const useStyles = makeStyles((theme) => ({
   image: {
     backgroundImage: 'url(https://source.unsplash.com/random)',
     backgroundRepeat: 'no-repeat',
-    backgroundColor: '#ccc',
+    backgroundColor:
+      theme.palette.type === 'light'
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
     backgroundSize: 'cover',
     backgroundPosition: 'center',
   },
@@ -55,7 +72,7 @@ export default function SignInSide() {
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Acesse a sua conta
+            Sign in
           </Typography>
           <form className={classes.form} noValidate>
             <TextField
@@ -64,7 +81,7 @@ export default function SignInSide() {
               required
               fullWidth
               id="email"
-              label="E-mail"
+              label="Email Address"
               name="email"
               autoComplete="email"
               autoFocus
@@ -75,10 +92,14 @@ export default function SignInSide() {
               required
               fullWidth
               name="password"
-              label="Senha"
+              label="Password"
               type="password"
               id="password"
               autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
             />
             <Button
               type="submit"
@@ -87,20 +108,23 @@ export default function SignInSide() {
               color="primary"
               className={classes.submit}
             >
-              Entrar
+              Sign In
             </Button>
             <Grid container>
               <Grid item xs>
                 <Link href="#" variant="body2">
-                  Não tem uma conta? Cadastre-se
+                  Forgot password?
                 </Link>
               </Grid>
               <Grid item>
                 <Link href="#" variant="body2">
-                  Crie sua conta
+                  {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
             </Grid>
+            <Box mt={5}>
+              <Copyright />
+            </Box>
           </form>
         </div>
       </Grid>
