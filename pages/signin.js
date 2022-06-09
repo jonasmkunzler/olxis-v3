86e1abd70445884d7ccadde02c7cbbdfd65c19d4
jonasmkunzler <jonasmkunzler@gmail.com>
2022-06-09 13:59:19 -0300

    1 file modified

--- pages/signin.js
+++ pages/signin.js
@@ -37,22 +37,22 @@ const useStyles = makeStyles((theme) => ({
     backgroundPosition: 'center',
   },
   paper: {
-    margin: theme.spacing(8, 4),
+    /* margin: theme.spacing(8, 4), */
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
   },
   avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
+    /* margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main, */
   },
   form: {
     width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
+    /*  marginTop: theme.spacing(1), */
   },
-  submit: {
+  /*  submit: {
     margin: theme.spacing(3, 0, 2),
-  },
+  }, */
 }));
 
 export default function SignInSide() {
